faf76452088e2e10eff9e98ef8787e0ea0e547d0
Wilmar <s1137070@student.hsleiden.nl>
2023-03-02 15:41:18 +0100

    Adds functional http service.

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { jsonIgnoreReplacer } from "json-ignore";
+import { UserService } from "./user.service";
+import { environment } from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private apiPath: string = environment.apiUrl;
+
+  constructor(private http: HttpClient, private userService: UserService) { }
+
+  get(destination: string) {
+    return this.http.get<any>(this.apiPath + destination, this.getRequestOptions())
+  }
+
+  post(destination: string, body: any) {
+    body = (JSON.stringify(body, jsonIgnoreReplacer));
+
+    return this.http.post<any>(this.apiPath + destination, body, this.getRequestOptions());
+  }
+
+  delete(destination: string) {
+    return this.http.delete(this.apiPath + destination, this.getRequestOptions())
+  }
+
+  put(destination: string, body: any) {
+    body = (JSON.stringify(body, jsonIgnoreReplacer));
+
+    return this.http.put(this.apiPath + destination, body, this.getRequestOptions())
+  }
+
+  private getRequestOptions() {
+
+    let headers = new HttpHeaders({'Content-Type': 'application/json'});
+
+    if (this.userService.getJwtToken() != undefined) {
+      headers = headers.append('Authorization', "Bearer " + this.userService.getJwtToken().replace("\"", ""))
+    }
+
+    let requestOptions: Object = {
+      observe: 'response',
+      responseType: 'json',
+      headers: headers
+    };
+    return requestOptions;
+  }
+}
