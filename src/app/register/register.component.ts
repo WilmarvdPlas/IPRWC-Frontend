9717d5375a0155d6ae34390259fc7c92f56a699c
Wilmar <s1137070@student.hsleiden.nl>
2023-03-03 11:37:43 +0100

    Adds basic functionality to register component.

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  passwordRepeat: string = '';
+
+  registerCalled = false;
+
+  register() {
+    this.registerCalled = true;
+    if (this.passwordSufficient() && this.emailSufficient() && this.nameSufficient() && this.passwordRepeatSufficient()) {
+
+    }
+  }
+
+  passwordSufficient() : boolean {
+    const passwordRegex = new RegExp("(?=^.{8,}$)(?=.*\\d)(?=.*[!@#$%^&*]+)(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$");
+    return passwordRegex.test(this.password);
+  }
+
+  emailSufficient() : boolean {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return emailRegex.test(this.email)
+  }
+
+  nameSufficient() {
+    return this.name.length > 0;
+  }
+
+  passwordRepeatSufficient() {
+    return this.password == this.passwordRepeat;
+  }
+
 }
