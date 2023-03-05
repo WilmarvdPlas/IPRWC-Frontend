36b2094dd922e3a04b349c14fb41d936fbaad667
Wilmar <s1137070@student.hsleiden.nl>
2023-03-05 23:05:45 +0100

    Add archived field to account model.

--- src/app/models/account.model.ts
+++ src/app/models/account.model.ts
@@ -5,15 +5,17 @@ export class Account {
   password?: string;
   name?: string;
   administrator?: boolean;
+  archived?: boolean;
   dateCreated?: Date;
 
-  constructor(id?: string, email?: string, password?: string, name?: string, administrator?: boolean, dateCreated?: Date) {
+  constructor(id?: string, email?: string, password?: string, name?: string, administrator?: boolean, archived?: boolean, dateCreated?: Date) {
     this.id = id;
     this.email = email;
     this.password = password;
     this.name = name;
     this.administrator = administrator;
     this.dateCreated = dateCreated;
+    this.archived = archived;
   }
 
 }
