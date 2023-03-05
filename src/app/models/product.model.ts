ade8416edf872ed602e5dabd86170c64230769ac
Wilmar <s1137070@student.hsleiden.nl>
2023-03-05 23:05:29 +0100

    Add description and archived fields to product model.

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,19 +2,23 @@ export class Product {
 
   id?: string;
   name?: string;
+  description?: string;
   price?: number;
   image?: string;
   stock?: number;
   discountPercentage?: number;
+  archived?: boolean;
   dateCreated?: Date;
 
-  constructor(id?: string, name?: string, price?: number, image?: string, stock?: number, discountPercentage?: number, dateCreated?: Date) {
+  constructor(id?: string, name?: string, description?: string, price?: number, image?: string, stock?: number, discountPercentage?: number, archived?: boolean, dateCreated?: Date) {
     this.id = id;
     this.name = name;
+    this.description = description;
     this.price = price;
     this.image = image;
     this.stock = stock;
     this.discountPercentage = discountPercentage;
+    this.archived = archived;
     this.dateCreated = dateCreated;
   }
 
