f0dfdd2ce76bbe8b828ce04d6708332ca060a330
Wilmar <s1137070@student.hsleiden.nl>
2023-03-07 20:12:19 +0100

    Add extra NbModules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,19 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
 import {
-    NbAccordionModule,
-    NbAlertModule, NbButtonGroupModule, NbButtonModule, NbCardModule, NbIconModule, NbInputModule,
-    NbLayoutModule, NbListModule, NbTabsetModule,
-    NbThemeModule,
-    NbToastrModule
+  NbAccordionModule,
+  NbAlertModule, NbButtonGroupModule, NbButtonModule, NbCardModule, NbFormFieldModule, NbIconModule, NbInputModule,
+  NbLayoutModule, NbListModule, NbSidebarModule, NbTableModule, NbTabsetModule,
+  NbThemeModule,
+  NbToastrModule
 } from "@nebular/theme";
 import {NbEvaIconsModule} from "@nebular/eva-icons";
 import {UserService} from "./services/user.service";
 import {HttpService} from "./services/http.service";
 import { FooterComponent } from './footer/footer.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { ProductManagementComponent } from './administration/product-management/product-management.component';
+import { ProductEditComponent } from './administration/product-management/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
@@ -30,29 +33,35 @@ import { FooterComponent } from './footer/footer.component';
     RegisterComponent,
     NavbarComponent,
     PageNotFoundComponent,
-    FooterComponent
+    FooterComponent,
+    AdministrationComponent,
+    ProductManagementComponent,
+    ProductEditComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        NbThemeModule.forRoot({name: 'dark'}),
-        NbButtonModule,
-        NbIconModule,
-        NbEvaIconsModule,
-        NbLayoutModule,
-        NbToastrModule.forRoot({limit: 3}),
-        NbAlertModule,
-        FormsModule,
-        MatIconModule,
-        NbButtonGroupModule,
-        NbCardModule,
-        NbAccordionModule,
-        NbInputModule,
-        NbListModule,
-        NbTabsetModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'dark'}),
+    NbButtonModule,
+    NbIconModule,
+    NbEvaIconsModule,
+    NbLayoutModule,
+    NbToastrModule.forRoot({limit: 3}),
+    NbAlertModule,
+    FormsModule,
+    MatIconModule,
+    NbButtonGroupModule,
+    NbCardModule,
+    NbAccordionModule,
+    NbInputModule,
+    NbListModule,
+    NbTabsetModule,
+    NbSidebarModule.forRoot(),
+    NbTableModule,
+    NbFormFieldModule
+  ],
   providers: [UserService, HttpService],
   bootstrap: [AppComponent]
 })
