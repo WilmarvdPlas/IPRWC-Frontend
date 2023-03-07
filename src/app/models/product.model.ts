3d05b892044aafff8b282cfba58b477c999f1a19
Wilmar <s1137070@student.hsleiden.nl>
2023-03-07 20:46:44 +0100

    Change image field name to imageLink.

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -4,18 +4,18 @@ export class Product {
   name?: string;
   description?: string;
   priceEuro?: number;
-  image?: string;
+  imageLink?: string;
   stock?: number;
   discountPercentage?: number;
   archived?: boolean;
   dateCreated?: Date;
 
-  constructor(id?: string, name?: string, description?: string, priceEuro?: number, image?: string, stock?: number, discountPercentage?: number, archived?: boolean, dateCreated?: Date) {
+  constructor(id?: string, name?: string, description?: string, priceEuro?: number, imageLink?: string, stock?: number, discountPercentage?: number, archived?: boolean, dateCreated?: Date) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.priceEuro = priceEuro;
-    this.image = image;
+    this.imageLink = imageLink;
     this.stock = stock;
     this.discountPercentage = discountPercentage;
     this.archived = archived;
