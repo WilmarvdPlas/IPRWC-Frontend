226b547e098a67e972c04b95050e856eaf121492
Wilmar <s1137070@student.hsleiden.nl>
2023-03-08 17:35:31 +0100

    Add fully functional product management component.

--- src/app/administration/product-management/product-management.component.ts
+++ src/app/administration/product-management/product-management.component.ts
@@ -1,19 +1,30 @@
-import {AfterViewChecked, Component, QueryList, ViewChildren} from '@angular/core';
+import {AfterViewChecked, Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Product} from "../../models/product.model";
 import {ProductEditComponent} from "./product-edit/product-edit.component";
+import {HttpService} from "../../services/http.service";
+import {NbToastrService} from "@nebular/theme";
 
 @Component({
   selector: 'app-product-management',
   templateUrl: './product-management.component.html',
   styleUrls: ['./product-management.component.scss']
 })
-export class ProductManagementComponent implements AfterViewChecked {
+export class ProductManagementComponent implements AfterViewChecked, OnInit {
 
   @ViewChildren(ProductEditComponent) productEditComponents?: QueryList<ProductEditComponent>;
   createProductComponent: any;
   editProductComponent: any;
 
+  editingProduct = false;
+
+  products?: Product[];
   selectedProduct?: Product;
+
+  constructor(private httpService: HttpService, private toastrService: NbToastrService) {}
+
+  ngOnInit() {
+    this.setProducts()
+  }
 
   ngAfterViewChecked() {
     this.createProductComponent = this.productEditComponents?.toArray()[0];
@@ -27,5 +38,35 @@ export class ProductManagementComponent implements AfterViewChecked {
   cancelProduct() {
     this.createProductComponent?.clear();
   }
+
+  editProduct(product: Product) {
+    this.editProductComponent.product = product;
+    this.editProductComponent.addedStock = 0;
+    this.editingProduct = true;
+  }
+
+  saveEditedProduct() {
+    this.editProductComponent.save();
+    this.editingProduct = false;
+  }
+
+  setProducts() {
+    this.httpService.get('product').subscribe({
+      next: (response) => { this.products = response.body },
+      error: () => { this.toastrService.danger('Producten konden niet opgehaald worden.', 'Error'); }
+    })
+  }
+
+  deleteProduct() {
+    this.selectedProduct!.archived = true;
+    this.httpService.post('product', this.selectedProduct).subscribe({
+      next: () => {
+        this.toastrService.success('Product succesvol verwijderd.', 'Succes');
+        this.setProducts();
+      },
+      error: () => { this.toastrService.danger('Product kon niet verwijderd worden.', 'Succes'); }
+    })
+  }
+
 
 }
