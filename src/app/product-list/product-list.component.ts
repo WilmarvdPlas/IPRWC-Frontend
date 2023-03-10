9fb19aa0695b9018609fa74f556e29b48c31a637
Wilmar <s1137070@student.hsleiden.nl>
2023-03-10 16:06:17 +0100

    Add buttons to inspect without- and add to cart with functionality.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,9 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Product} from "../models/product.model";
+import {HttpService} from "../services/http.service";
+import {CartProduct} from "../models/cart-product.model";
+import {UserService} from "../services/user.service";
+import {NbToastrService} from "@nebular/theme";
 
 @Component({
   selector: 'app-product-list',
@@ -16,13 +20,21 @@ export class ProductListComponent {
   @Output() updateSelectedProduct = new EventEmitter<Product>();
   @Output() editProduct = new EventEmitter<Product>();
 
-  constructor() {}
+  constructor(private httpService: HttpService, private userService: UserService, private toastrService: NbToastrService) {}
 
   setSelectedProduct(product: Product) {
     if (this.type == 'MANAGEMENT') {
       this.selectedProduct == product ? this.selectedProduct = undefined : this.selectedProduct = product;
       this.updateSelectedProduct.emit(this.selectedProduct);
     }
+  }
+
+  addToCart(product: Product) {
+    let cartProduct = new CartProduct(undefined, product, this.userService.getActiveAccount() );
+    this.httpService.post('cart_product', cartProduct).subscribe({
+      next: () => { this.toastrService.success('"' + product.name + '" toegevoegd aan winkelwagen.', 'Succes'); },
+      error: () => { this.toastrService.danger('Product kon niet toegevoegd worden aan winkelwagen.', 'Error') }
+    })
   }
 
 }
