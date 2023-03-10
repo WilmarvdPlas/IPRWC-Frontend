b7d4ad50828bb2eaeda33102e8626daaba36b530
Wilmar <s1137070@student.hsleiden.nl>
2023-03-10 15:36:01 +0100

    Add products component.

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from "../services/http.service";
+import {Product} from "../models/product.model";
+import {NbToastrService} from "@nebular/theme";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  products?: Product[]
+  filteredProducts?: Product[]
+
+  constructor(private httpService: HttpService, private toastrService: NbToastrService) {}
+
+  ngOnInit() {
+    this.setProducts()
+  }
+
+  setProducts() {
+    this.httpService.get('product').subscribe({
+      next: (response) => { this.products = response.body },
+      error: () => { this.toastrService.danger('Producten konden niet opgehaald worden.', 'Error'); }
+    })
+  }
+}
