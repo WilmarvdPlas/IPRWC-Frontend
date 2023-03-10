6b816f309717a2e8a7f50e675971cd2ac5c18b1e
Wilmar <s1137070@student.hsleiden.nl>
2023-03-11 00:09:58 +0100

    Add count field to cart product.

--- src/app/models/cart-product.model.ts
+++ src/app/models/cart-product.model.ts
@@ -4,12 +4,14 @@ import {Account} from "./account.model";
 export class CartProduct {
 
   id?: string;
+  count?: number;
   product?: Product;
   account?: Account;
   dateCreated?: Date;
 
-  constructor(id?: string, product?: Product, account?: Account, dateCreated?: Date) {
+  constructor(id?: string, count?: number, product?: Product, account?: Account, dateCreated?: Date) {
     this.id = id;
+    this.count = count;
     this.product = product;
     this.account = account;
     this.dateCreated = dateCreated;
