0039b09197e0ef038e804c5a063098f16b114938
Wilmar <s1137070@student.hsleiden.nl>
2023-03-16 16:10:13 +0100

    Load list of accounts and make accounts selectable.

--- src/app/administration/account-management/account-management.component.ts
+++ src/app/administration/account-management/account-management.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpService} from "../../services/http.service";
+import {Account} from "../../models/account.model";
+import {NbToastrService} from "@nebular/theme";
 
 @Component({
   selector: 'app-account-management',
   templateUrl: './account-management.component.html',
   styleUrls: ['./account-management.component.scss']
 })
-export class AccountManagementComponent {
+export class AccountManagementComponent implements OnInit {
+
+  accounts?: Account[];
+  selectedAccount?: Account;
+
+  constructor(private httpService: HttpService, private toastrService: NbToastrService) {}
+
+  ngOnInit() {
+    this.setAccounts();
+  }
+
+  setAccounts() {
+    this.httpService.get('account').subscribe({
+      next: (response) => { this.accounts = response.body; },
+      error: () => { this.toastrService.danger('Accounts konden niet opgehaald worden.', 'Error');}
+    })
+  }
 
 }
