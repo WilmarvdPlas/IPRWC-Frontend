c67f56b7749114e73ec78ba7ce57dde824af40b5
Wilmar <s1137070@student.hsleiden.nl>
2023-03-16 21:26:28 +0100

    Add delete and authorise account options.

---
+++ src/app/administration/account-management/account-management-dialog/account-management-dialog.component.ts
@@ -0,0 +1,58 @@
+import {Component} from '@angular/core';
+import {NbDialogRef, NbToastrService} from "@nebular/theme";
+import {Account} from "../../../models/account.model";
+import {HttpService} from "../../../services/http.service";
+
+@Component({
+  selector: 'app-account-management-dialog',
+  templateUrl: './account-management-dialog.component.html',
+  styleUrls: ['./account-management-dialog.component.scss']
+})
+export class AccountManagementDialogComponent {
+
+  actionText?: string;
+  consequenceText?: string;
+  type?: string;
+  account?: Account;
+
+  constructor(private dialogRef: NbDialogRef<AccountManagementDialogComponent>, private httpService: HttpService, private toastrService: NbToastrService) {}
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  confirm() {
+    if (this.type == 'ADMINISTRATOR') {
+      this.confirmAdministrator();
+    } else if (this.type == 'DELETE') {
+      this.confirmDelete();
+    }
+  }
+
+  confirmAdministrator() {
+    this.httpService.put('account/' + this.account?.id + '/administrator', undefined).subscribe({
+      next: () => {
+        this.dialogRef.close();
+        this.toastrService.success('Account succesvol als beheerder toegewezen.', 'Succes');
+      },
+      error: () => {
+        this.dialogRef.close();
+        this.toastrService.danger('Account kon niet als beheerder toegewezen worden.', 'Error');
+      }
+    })
+  }
+
+  confirmDelete() {
+    this.httpService.delete('account/' + this.account?.id).subscribe({
+      next: () => {
+        this.dialogRef.close();
+        this.toastrService.success('Account is succesvol verwijderd.', 'Succes');
+      },
+      error: (error) => {
+        this.dialogRef.close();
+        this.toastrService.danger('Account kon niet verwijderd worden.', 'Error');
+      }
+    })
+  }
+
+}
--- src/app/administration/account-management/account-management.component.ts
+++ src/app/administration/account-management/account-management.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from "../../services/http.service";
 import {Account} from "../../models/account.model";
-import {NbToastrService} from "@nebular/theme";
+import {NbDialogService, NbToastrService} from "@nebular/theme";
+import {AccountManagementDialogComponent} from "./account-management-dialog/account-management-dialog.component";
 
 @Component({
   selector: 'app-account-management',
@@ -13,7 +14,7 @@ export class AccountManagementComponent implements OnInit {
   accounts?: Account[];
   selectedAccount?: Account;
 
-  constructor(private httpService: HttpService, private toastrService: NbToastrService) {}
+  constructor(private httpService: HttpService, private toastrService: NbToastrService, private dialogService: NbDialogService) {}
 
   ngOnInit() {
     this.setAccounts();
@@ -23,6 +24,33 @@ export class AccountManagementComponent implements OnInit {
     this.httpService.get('account').subscribe({
       next: (response) => { this.accounts = response.body; },
       error: () => { this.toastrService.danger('Accounts konden niet opgehaald worden.', 'Error');}
+    })
+  }
+
+  openAdministratorDialog() {
+    this.dialogService.open(AccountManagementDialogComponent,
+      {context: {
+          account: this.selectedAccount,
+          actionText: 'U staat op het punt om aan het volgende account beheerder rechten te verlenen:',
+          consequenceText: 'Als u aan dit account beheeder rechten verleent krijgt dit account toegang tot het beheren van producten, accounts en bestellingen. ' +
+            'Deze rechten kunnen via de in-app interface niet ontnomen worden.',
+          type: 'ADMINISTRATOR'
+      }})
+      .onClose.subscribe(() => {
+      this.setAccounts();
+    })
+  }
+
+  openDeleteDialog() {
+    this.dialogService.open(AccountManagementDialogComponent,
+      {context: {
+          account: this.selectedAccount,
+          actionText: 'U staat op het punt om het volgende account te verwijderen:',
+          consequenceText: 'Als u dit account verwijderd, zal dit account permanent uit het systeem verdwijnen. Er zal geen mogelijkheid bestaan om dit account terug te halen.',
+          type: 'DELETE'
+        }})
+      .onClose.subscribe(() => {
+      this.setAccounts();
     })
   }
 
