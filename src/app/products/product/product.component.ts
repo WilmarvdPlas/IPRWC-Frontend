a5e522efa0f739a6f358a26513cadadd310572ce
Wilmar <s1137070@student.hsleiden.nl>
2023-03-19 14:05:04 +0100

    Add fully functional product component.

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from "../../models/product.model";
+import {HttpService} from "../../services/http.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CartProduct} from "../../models/cart-product.model";
+import {NbToastrService} from "@nebular/theme";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+  product?: Product;
+  routeProductId?: string;
+
+  constructor(private httpService: HttpService, private route: ActivatedRoute, private toastrService: NbToastrService, private userService: UserService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.routeProductId = params['id'];
+      this.setProduct();
+    })
+  }
+
+  setProduct() {
+    this.httpService.get('product/' + this.routeProductId).subscribe({
+      next: (response) => { this.product = response.body; },
+      error: (error) => { console.log(error); }
+    })
+  }
+
+  addProductToCart() {
+    let cartProduct = new CartProduct(undefined, 1, this.product, this.userService.getActiveAccount());
+
+    this.httpService.post('cart_product', cartProduct).subscribe({
+      next: () => { this.toastrService.success('"' + this.product?.name + '" added to cart.', 'Success'); },
+      error: (error) => {
+        error.status == 406
+          ? this.toastrService.danger("You can't add more than 100 copies of this item to your cart." , 'Error')
+          : this.toastrService.danger('Item could not be added to cart.', 'Error');
+      }
+    })
+  }
 
 }
