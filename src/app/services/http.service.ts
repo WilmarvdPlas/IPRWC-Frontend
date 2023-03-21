d6cd3191a72870e7a6a97256a86baf419975e257
Wilmar <s1137070@student.hsleiden.nl>
2023-03-21 21:57:11 +0100

    Refactor apiPath to apiUrl.

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -8,28 +8,28 @@ import {environment} from "../../environments/environment";
 })
 export class HttpService {
 
-  private apiPath: string = environment.apiUrl;
+  private apiUrl: string = environment.apiUrl;
 
   constructor(private http: HttpClient, private userService: UserService) { }
 
   get(destination: string) {
-    return this.http.get<any>(this.apiPath + destination, this.getRequestOptions());
+    return this.http.get<any>(this.apiUrl + destination, this.getRequestOptions());
   }
 
   post(destination: string, body: any) {
     body = JSON.stringify(body);
 
-    return this.http.post<any>(this.apiPath + destination, body, this.getRequestOptions());
+    return this.http.post<any>(this.apiUrl + destination, body, this.getRequestOptions());
   }
 
   delete(destination: string) {
-    return this.http.delete(this.apiPath + destination, this.getRequestOptions());
+    return this.http.delete(this.apiUrl + destination, this.getRequestOptions());
   }
 
   put(destination: string, body: any) {
     body = JSON.stringify(body);
 
-    return this.http.put<any>(this.apiPath + destination, body, this.getRequestOptions());
+    return this.http.put<any>(this.apiUrl + destination, body, this.getRequestOptions());
   }
 
   authorisedFilter(statusCode: number) {
